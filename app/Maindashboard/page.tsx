fecb339b0bb484f24a009534580b143dae9b43e1
AJprSINGH <ajit.12008255@lpu.in>
2025-06-27 19:31:48 +0530

    second commit

--- app/Maindashboard/page.tsx
+++ app/Maindashboard/page.tsx
@@ -1,25 +1,20 @@
-// app/dashboard/page.tsx
+// app/Maindashboard/page.tsx
 
 import MainScreen from "../content/Dashboard/MainScreen";
 import Header from '../../components/Header/Header'
 import '../globals.css'
 
-// export default function DashboardPage() {
-//   console.log("DashboardPage rendered");
-//   return <MainScreen />;
-// }
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-
-  return (<>
-
-    <Header />
-    {/* test starts here  */}
-    <div className="contentDiv w-full max-w-[1445px] max-md:max-w-full">
-      <div className="mainDiv">
-        <main style={{ padding: '1rem' }}> <MainScreen /></main>
+export default function DashboardPage() {
+  return (
+    <>
+      <Header />
+      <div className="contentDiv w-full max-w-[1445px] max-md:max-w-full">
+        <div className="mainDiv">
+          <main style={{ padding: '1rem' }}>
+            <MainScreen />
+          </main>
+        </div>
       </div>
-    </div>
-
-  </>)
+    </>
+  );
 }
